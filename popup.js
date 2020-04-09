d5e4e8faeeeccd996189a5951d4565feea82c926
Daniel Rychlewski <daniel.rychlewski@web.de>
2020-04-09 17:10:06 +0200

    can display url history in background page, not just delete

--- popup.js
+++ popup.js
@@ -1,12 +1,20 @@
 'use strict';
 
+let displaySites = document.getElementById('displaySites');
 let clearSites = document.getElementById('clearSites');
 let settings = document.getElementById('settings');
+
+displaySites.onclick = function(element) {
+  chrome.storage.local.get('urlHistory', function (result) {
+    var bkg = chrome.extension.getBackgroundPage();
+    bkg.console.log(new Set(result.urlHistory));
+  });
+};
 
 clearSites.onclick = function(element) {
   chrome.storage.local.set({'urlHistory': [...new Set()]}, function() {
     var bkg = chrome.extension.getBackgroundPage();
-    bkg.console.log("urlHistory set has been cleared");
+    bkg.console.log("urlHistory has been cleared");
   });
 };
 
