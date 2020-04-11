08ff80f94a8775b95583c6a1d3a9364b84658c35
Daniel Rychlewski <daniel.rychlewski@web.de>
2020-04-11 13:19:57 +0200

    whitelist, max video length blueprints

--- global.js
+++ global.js
@@ -1,10 +1,12 @@
 function setupVariables() {
     chrome.storage.local.set({'urlHistory': []});
     chrome.storage.local.set({'includePlaylists': false});
-    chrome.storage.local.set({'clearSitesAtLoop': true});
+    chrome.storage.local.set({'clearSitesAtLoop': false});
     chrome.storage.local.set({'toggleAutoplayClearsSites': true});
     chrome.storage.local.set({'backgroundRedirectEntry': 1});
-    chrome.storage.local.set({'blacklist': ['https://www.youtube.com/watch?v=EXAMPLE1234']});
+    chrome.storage.local.set({'maximumVideoLength': 7});
+    chrome.storage.local.set({'blacklist': ['https://www.youtube.com/watch?v=BLACKLISTED']});
+    chrome.storage.local.set({'whitelist': ['https://www.youtube.com/watch?v=WHITELISTED']});
     chrome.storage.local.set({'toggleCheckInterval': 500});
     chrome.storage.local.set({'videoRecommendationsInterval': 100});
     chrome.storage.local.set({'videoEndInterval': 100});
--- options.js
+++ options.js
@@ -18,23 +18,25 @@ function setSliders() {
 }
 
 function setInputs() {
-    ['backgroundRedirectEntry', 'toggleCheckInterval', 'videoRecommendationsInterval', 'videoEndInterval'].forEach(key => {
+    ['backgroundRedirectEntry', 'maximumVideoLength', 'toggleCheckInterval', 'videoRecommendationsInterval', 'videoEndInterval'].forEach(key => {
         chrome.storage.local.get([key],
             function(data) {
                 document.getElementById(key).value = data[key];
             }
         );
     });
-    setBlacklistEntries();
+    setFilterlistEntries();
 }
 
-function setBlacklistEntries() {
-    let blacklistKey = 'blacklist';
-    chrome.storage.local.get([blacklistKey],
-        function(data) {
-            document.getElementById(blacklistKey).value = data[blacklistKey].join("\n");
-        }
-    );
+function setFilterlistEntries() {
+    let filterlistKeys = ['blacklist', 'whitelist'];
+    filterlistKeys.forEach(key => {
+        chrome.storage.local.get([key],
+            function(data) {
+                document.getElementById(key).value = data[key].join("\n");
+            }
+        );
+    });
 }
 
 function evaluateSliders() {
@@ -44,34 +46,38 @@ function evaluateSliders() {
 }
 
 function evaluateInputs() {
-    ['backgroundRedirectEntry', 'toggleCheckInterval', 'videoRecommendationsInterval', 'videoEndInterval'].forEach(key => {
+    ['backgroundRedirectEntry', 'maximumVideoLength', 'toggleCheckInterval', 'videoRecommendationsInterval', 'videoEndInterval'].forEach(key => {
         if (document.getElementById(key).value && !isNaN(document.getElementById(key).value)) {
             chrome.storage.local.set({[key]: document.getElementById(key).value});
         }
     });
-    evaluateBlacklistEntries();
+    evaluateFilterlistEntries();
 }
 
-function evaluateBlacklistEntries() {
-    let blacklistKey = 'blacklist';
-    let blacklistValue = document.getElementById(blacklistKey).value;
-    if (blacklistValue) {
-        var blacklistLines = blacklistValue.split('\n');
-        let isValid = true;
-        blacklistLines.forEach(line => {
-            if (!line.startsWith("https://www.youtube.com/watch?")) {
-                isValid = false;
-            }
-        });
-        if (isValid) {
-            chrome.storage.local.set({[blacklistKey]: blacklistLines});
-
-            chrome.storage.local.get('urlHistory', function (result) {
-                blacklistLines.forEach(url => result.urlHistory.push(url));
-                chrome.storage.local.set({'urlHistory': result.urlHistory});
+function evaluateFilterlistEntries() {
+    let filterlistKeys = ['blacklist', 'whitelist'];
+    filterlistKeys.forEach(filterlistKey => {
+        let filterlistValue = document.getElementById(filterlistKey).value;
+        if (filterlistValue) {
+            var filterlistLines = filterlistValue.split('\n');
+            let isValid = true;
+            filterlistLines.forEach(line => {
+                if (!line.startsWith("https://www.youtube.com/watch?")) {
+                    isValid = false;
+                }
             });
+            if (isValid) {
+                chrome.storage.local.set({[filterlistKey]: filterlistLines});
+
+                if (filterlistKey === 'blacklist') {
+                    chrome.storage.local.get('urlHistory', function (result) {
+                        filterlistLines.forEach(url => result.urlHistory.push(url));
+                        chrome.storage.local.set({'urlHistory': result.urlHistory});
+                    });
+                }
+            }
         }
-    }
+    });
 }
 
 function submit() {
