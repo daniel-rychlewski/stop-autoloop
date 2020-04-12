ebc8af270612281881e55b0188085eab77df9479
Daniel Rychlewski <daniel.rychlewski@web.de>
2020-04-12 12:59:30 +0200

    blacklist needn't be added in addUrl

--- addUrl.js
+++ addUrl.js
@@ -1,10 +1,4 @@
 chrome.storage.local.get('urlHistory', function (result) {
-    if (result.urlHistory.length === 0) {
-        // autoplay toggle might have been re-activated, need to add blacklist
-        chrome.storage.local.get('blacklist', function (blacklist) {
-            blacklist.blacklist.forEach(url => result.urlHistory.push(url));
-        });
-    }
     result.urlHistory.push(location.href);
     chrome.storage.local.set({'urlHistory': result.urlHistory});
 });
